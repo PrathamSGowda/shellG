f2a88eae43da9097e9c614df3bf19ef9b716b265
Pratham S Gowda <prathampsg2007@gmail.com>
2026-04-17 23:03:05 +0530

    Implement REPL and exit

--- src/main.c
+++ src/main.c
@@ -1,18 +1,27 @@
 #include <stdio.h>
+#include <string.h>
 
 int main()
 { 
+    while(1) // keeps the shell running (REPL)
+    {
 /*
     printing a prompt for shell
 */
     printf("$ ");
 
+    char ch[100];
+    scanf(" %[^\n]s",ch);
+/*
+    implementing exit from the loop
+*/
+    if (strcmp(ch, "exit") == 0)
+            break;
 /*
     printing error messages for invalid commands
 */
-    char ch[100];
-    scanf("%[^\n]s",ch);
     printf("%s: command not found\n", ch);
+    }
 
     return 0;
 }
