612f362b0c91d68cd38e444afc5303712399d58e
Pratham S Gowda <prathampsg2007@gmail.com>
2026-06-02 23:49:04 +0530

    changed to test the lexer

--- src/main.c
+++ src/main.c
@@ -1,43 +1,37 @@
+/*********************************************/
+/*                                           */
+/*  main.c                                   */
+/*                                           */
+/*  By: Pratham <prathampsg2007@gmail.com>   */                               
+/*                                           */
+/*  Created: 17/04/2026 by Pratham           */
+/*  Updated: 02/06/2026 by Pratham           */
+/*                                           */
+/*********************************************/
+
 #include <stdio.h>
 #include <string.h>
-#include "builtin.h"
+#include "lexer.h"
 
 int main()
-{ 
-    while(1) // keeps the shell running (REPL)
+{
+    while (1)
     {
-
-    printf("[shellG] "); // prompt for shell
-
-    char ch[1024];
-    scanf(" %[^\n]s",ch);
-
-    if (strcmp(ch, "exit") == 0)  // exit command
+        char ch[1024];
+        printf("[shellG] ");
+        fgets(ch, sizeof(ch), stdin);
+        if (strcmp(ch, "exit\n") == 0)
             break;
-
-    else if (strncmp(ch,"echo ",5) == 0)
-    {
-        run_echo(ch);
+        Lexer lexer = {.start = ch,.cur = ch,.tokens = NULL,.tok_count = 0,.capacity = 0};
+        lex(&lexer);
+        for (int i = 0; i < lexer.tok_count; i++)
+        {
+            printf("%s",token_to_string(lexer.tokens[i].type));
+            if (lexer.tokens[i].val != NULL)
+                printf(" -> %s",lexer.tokens[i].val);
+            printf("\n");
+        }
+        free_lexer(&lexer);
     }
-
-    else if (strncmp(ch,"type ",5) == 0)
-    {
-        run_type(ch);
-    }
-
-    else if (strcmp(ch,"pwd") == 0)
-    {
-        run_pwd(ch);
-    }
-
-    else if (strncmp(ch,"cd ",3) == 0)
-    {
-        run_cd(ch);
-    }
-
-    else
-        printf("%s: command not found\n",ch);
-    }
-
     return 0;
 }
